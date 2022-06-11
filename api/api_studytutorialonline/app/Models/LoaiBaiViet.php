d34675233de077f8dc8320b554c095b7d5abd693
Lai Khải <56122788+LaiKhai@users.noreply.github.com>
2022-06-11 17:58:45 +0700

    @20220611 create migration data - Khai

---
+++ api/api_studytutorialonline/app/Models/LoaiBaiViet.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LoaiBaiViet extends Model
+{
+    use HasFactory;
+    protected $table = 'loai_bai_viets';
+    protected $fillable = [
+        'id',
+        'ten_loai',
+        'trang_thai'
+    ];
+}
