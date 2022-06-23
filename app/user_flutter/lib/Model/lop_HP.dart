deae0557bcfe0300ce6ae6ca3e06597fb118cfbd
Lê Duy <duy.lethanh@di4l.vn>
2022-06-23 12:29:02 +0700

    load danh sách lớp,

---
+++ app/user_flutter/lib/Model/lop_HP.dart
@@ -0,0 +1,83 @@
+class lop_HP {
+  List<Lophocphan>? lophocphan;
+
+  lop_HP({this.lophocphan});
+
+  lop_HP.fromJson(Map<String, dynamic> json) {
+    if (json['lophocphan'] != null) {
+      lophocphan = <Lophocphan>[];
+      json['lophocphan'].forEach((v) {
+        lophocphan!.add(new Lophocphan.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.lophocphan != null) {
+      data['lophocphan'] = this.lophocphan!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Lophocphan {
+  int? idSinhVien;
+  int? id;
+  int? idBoMon;
+  int? idLop;
+  Null? avt;
+  int? trangThai;
+  String? createdAt;
+  String? updatedAt;
+  String? hoTenSv;
+  String? tenMonHoc;
+  String? tenLop;
+
+  Lophocphan(
+      {this.idSinhVien,
+      this.id,
+      this.idBoMon,
+      this.idLop,
+      this.avt,
+      this.trangThai,
+      this.createdAt,
+      this.updatedAt,
+      this.hoTenSv,
+      this.tenMonHoc,
+      this.tenLop});
+
+  Lophocphan.fromJson(Map<String, dynamic> json) {
+    idSinhVien = json['id_sinh_vien'];
+    id = json['id'];
+    idBoMon = json['id_bo_mon'];
+    idLop = json['id_lop'];
+    if (json['avt'] == null) {
+      json['avt'] = 'assets/images/lophocphan/no_image.jpg';
+    } else {
+      avt = json['avt'];
+    }
+    trangThai = json['trang_thai'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+    hoTenSv = json['ho_ten_sv'];
+    tenMonHoc = json['ten_mon_hoc'];
+    tenLop = json['ten_lop'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id_sinh_vien'] = this.idSinhVien;
+    data['id'] = this.id;
+    data['id_bo_mon'] = this.idBoMon;
+    data['id_lop'] = this.idLop;
+    data['avt'] = this.avt;
+    data['trang_thai'] = this.trangThai;
+    data['created_at'] = this.createdAt;
+    data['updated_at'] = this.updatedAt;
+    data['ho_ten_sv'] = this.hoTenSv;
+    data['ten_mon_hoc'] = this.tenMonHoc;
+    data['ten_lop'] = this.tenLop;
+    return data;
+  }
+}
