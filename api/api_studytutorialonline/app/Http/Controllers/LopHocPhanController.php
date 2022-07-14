86dbde8dc419245dbb9e00e8954498818b8f8ed3
Lai Khải <56122788+LaiKhai@users.noreply.github.com>
2022-07-14 23:19:47 +0700

    @20220714 update api lophocphan- Khai

--- api/api_studytutorialonline/app/Http/Controllers/LopHocPhanController.php
+++ api/api_studytutorialonline/app/Http/Controllers/LopHocPhanController.php
@@ -143,7 +143,6 @@ class LopHocPhanController extends Controller
         $lopHocPhan->baikiemtra;
         $lopHocPhan->bomon;
         $lopHocPhan->baitap;
-        // $this->FixImg($lopHocPhan);
 
         $dsgv = DS_GiangVien::join('lop_hoc_phans', 'ds_giang_viens.id_lop_hoc_phan', '=', 'lop_hoc_phans.id')
             ->join('giang_viens', 'ds_giang_viens.id_giang_vien', '=', 'giang_viens.id')
@@ -156,7 +155,7 @@ class LopHocPhanController extends Controller
 
         $response = [
             'status' => true,
-            'lophocphan' => $lopHocPhan->avt,
+            'lophocphan' => $lopHocPhan,
             'dssv' => $dssv,
             'dsgv' => $dsgv
         ];
