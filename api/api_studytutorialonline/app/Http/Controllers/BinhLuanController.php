9f2cc64fe8282135c6910bf8e1290bda4661aeee
Lai Khải <56122788+LaiKhai@users.noreply.github.com>
2022-07-15 23:24:18 +0700

    @20220715 update api chi tiet bai kiem tra - Khai

--- api/api_studytutorialonline/app/Http/Controllers/BinhLuanController.php
+++ api/api_studytutorialonline/app/Http/Controllers/BinhLuanController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\BinhLuan;
+use App\Models\BaiViet;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -45,11 +46,13 @@ class BinhLuanController extends Controller
      */
     public function store(Request $request)
     {
+        $input['id_bai_viet'] = $request->input('id_bai_viet');
         $input['id_bai_tap'] = $request->input('id_bai_tap');
         $input['id_sinh_vien'] = $request->input('id_sinh_vien');
         $input['noi_dung'] = $request->input('noi_dung');
         $input['trang_thai'] = 1;
         $validator = Validator::make($input, [
+            'id_bai_viet' => ['required', 'max:255', 'integer'],
             'id_bai_tap' => ['required', 'max:255', 'integer'],
             'id_sinh_vien' => ['required', 'max:255', 'integer'],
             'noi_dung' => ['required', 'max:255', 'string'],
@@ -173,6 +176,14 @@ class BinhLuanController extends Controller
 
     public function binhluanwithidbaiviet(Request $request)
     {
+        $baiviet = BaiViet::find($request->input('id_bai_viet'));
+        if (empty($baiviet)) {
+            $response = [
+                'status' => false,
+                'message' => 'không tìm thấy lớp nào'
+            ];
+            return response()->json($response, 404);
+        }
         $binhluan = BinhLuan::join('bai_viets', 'binh_luans.id_bai_viet', '=', 'bai_viets.id')
             ->where('bai_viets.id', $request->input('id_bai_viet'))
             ->select('binh_luans.*', 'bai_viets.id as idBaiViet')
